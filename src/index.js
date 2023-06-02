52af3a0e41b236ee10cfcd24c29fb4ae78e8be1b
darya <dabespalova@mail.ru>
2023-06-02 12:04:38 +0400

    step 3

--- src/index.js
+++ src/index.js
@@ -11,5 +11,12 @@ export default (filepath1, filepath2) => {
     if (typeOf(filepath1) !== 'json' || typeOf(filepath2) !== 'json') {
         return 'this is incorrect format.'
     }
-    
+    const getFilePath = (file) => path.resolve(procces.cwd(), file);
+    const readFile = (filePath) => readFileSync(filePath, 'utf8');
+
+    const path1 = getFilePath(filepath1);
+    const data1 = readFile(path1);
+
+    const path2 = getFilePath(filepath2);
+    const data2 = readFile(path2);
 };
