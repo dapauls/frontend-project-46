267d0837c2a3fe3ff3986dd38c9da819604f00ab
darya <dabespalova@mail.ru>
2023-06-07 10:13:29 +0400

    step 5

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,18 +8,21 @@ const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
+let contentOfResultFile;
+let resultFile;
+beforeEach(() => {
+  resultFile = getFixturePath('result.txt');
+  contentOfResultFile = readFileSync(resultFile, 'utf8');
+});
+
 test('json files', () => {
   const fileOne = getFixturePath('file1.json');
   const fileTwo = getFixturePath('file2.json');
-  const resultFile = getFixturePath('result.txt');
-  const contentOfResultFile = readFileSync(resultFile, 'utf8');
   expect(genDiff(fileOne, fileTwo)).toBe(contentOfResultFile);
 });
 
 test('yaml files', () => {
   const fileOne = getFixturePath('file1.yml');
   const fileTwo = getFixturePath('file2.yml');
-  const resultFile = getFixturePath('result.txt');
-  const contentOfResultFile = readFileSync(resultFile, 'utf8');
   expect(genDiff(fileOne, fileTwo)).toBe(contentOfResultFile);
 });
