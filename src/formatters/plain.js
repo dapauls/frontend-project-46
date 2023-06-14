f4cfdef943f8ceb80c75ab1983428a93ce8c41be
darya <dabespalova@mail.ru>
2023-06-14 15:12:44 +0400

    step 7

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -4,31 +4,36 @@ const getRightValue = (value) => {
   if (_.isObject(value)) {
     return '[complex value]';
   }
+  if (typeof value === 'string') {
+    return `'${value}'`;
+  }
   return value;
 };
 
-const toPlain = (diff) => {
+const toPlain = (diff, parents = []) => {
   const diffWithoutUnchangedStatus = diff.filter(({ status }) => status !== 'unchanged');
   const result = diffWithoutUnchangedStatus.map(({ key, status, value }) => {
+    const newParents = parents.concat(key);
+    const newParentsStr = newParents.join('.');
     let string;
     switch (status) {
       case 'update':
-        string = `Property '${key}' was updated. From ${getRightValue(value.oldValue)} to ${getRightValue(value.NewValue)}\n`;
+        string = `Property '${newParentsStr}' was updated. From ${getRightValue(value.oldValue)} to ${getRightValue(value.newValue)}`;
         break;
       case 'removed':
-        string = `Property '${key}' was removed\n`;
+        string = `Property '${newParentsStr}' was removed`;
         break;
       case 'added':
-        string = `Property '${key}' was added with value: ${getRightValue(value)}\n`;
+        string = `Property '${newParentsStr}' was added with value: ${getRightValue(value)}`;
         break;
       case 'object':
-        return toPlain(value);
+        return toPlain(value, newParents);
       default:
-        string = 'Error!\n';
+        string = 'Error!';
     }
     return string;
   });
-  return result.join('');
+  return result.join('\n');
 };
 
 export default toPlain;
