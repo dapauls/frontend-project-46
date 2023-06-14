bbd9eeca6d0d94557726f544f11837f2248e4c20
darya <dabespalova@mail.ru>
2023-06-14 15:27:19 +0400

    step 8

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,6 +1,6 @@
 import toStylish from './stylish.js';
 import toPlain from './plain.js';
-// import toJSON from './json.js';
+import toJSON from './JSON.js';
 
 export default (diff, format) => {
   let result;
@@ -10,6 +10,9 @@ export default (diff, format) => {
       break;
     case 'plain':
       result = toPlain(diff);
+      break;
+    case 'json':
+      result = toJSON(diff);
       break;
     default:
       result = 'error';
